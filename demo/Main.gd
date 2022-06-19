54fb06f2ff2a2e5a948def2f14cdf3ea4ba12105
Hansem Ro <hansemro@outlook.com>
2022-06-19 00:54:52 -0700

    Add initial demo

---
+++ demo/Main.gd
@@ -0,0 +1,11 @@
+extends Node
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	set_process_input(true)
+
+func _input(event: InputEvent) -> void:
+	if (event is InputEventMouseMotion):
+		print(event.as_text())
+		#print(event.pen_inverted)
+		#print(event.get_pen_inverted())
