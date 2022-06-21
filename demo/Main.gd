d68fd30d3652cc0fdcd3bf5174acc12a5851571e
Hansem Ro <hansemro@outlook.com>
2022-06-21 01:12:22 -0700

    Update demo with visual feedback

--- demo/Main.gd
+++ demo/Main.gd
@@ -1,11 +1,28 @@
 extends Node
 
+var pen_inverted := false
+var pressed := false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_process_input(true)
 
 func _input(event: InputEvent) -> void:
-	if (event is InputEventMouseMotion):
+	if event is InputEventMouseMotion:
 		#print(event.as_text())
-		#print(event.pen_inverted)
-		print(event.get_pen_inverted())
+		pen_inverted = event.get_pen_inverted()
+		print(pen_inverted)
+	elif event is InputEventMouseButton:
+		pressed = event.pressed
+
+func _process(delta: float) -> void:
+	if pen_inverted:
+		if pressed:
+			VisualServer.set_default_clear_color(Color(0,1.0,0,1.0))
+		else:
+			VisualServer.set_default_clear_color(Color(0,0.6,0,1.0))
+	else:
+		if pressed:
+			VisualServer.set_default_clear_color(Color(1.0,0,0,1.0))
+		else:
+			VisualServer.set_default_clear_color(Color(0.6,0,0,1.0))
